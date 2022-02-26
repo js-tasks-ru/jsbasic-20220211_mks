da1df8c7546e8fd68d61da1172d43b8950542263
Kos Majorof <mks@medialit.ru>
2022-02-27 00:18:29 +0300

    Занятие 3. Задача 3.

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+
+  let arr = str.split('-');
+
+  let firstItem = arr.shift(0, 1);
+
+  arr.splice();
+
+  let mapped = arr.map(item => item.charAt(0).toUpperCase() + item.slice(1)).join('');
+
+  let value = firstItem + mapped;
+
+  return value;
 }
