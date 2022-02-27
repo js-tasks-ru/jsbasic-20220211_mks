d892e7404ca45e570dbce83c233f932ba27d4994
Kos Majorof <mks@medialit.ru>
2022-02-27 19:13:22 +0300

    Занятие 3. Задача 4.

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  const arr = [];
+
+  for (user of users) {
+    if (user.age <= age) {
+      arr.push(`${user.name}, ${user.balance}`);
+    }
+  }
+
+  return arr.join('\n');
 }
