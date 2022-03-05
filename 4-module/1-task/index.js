38475d8dffd0e5496a21657181d54e5f25f394ca
Kos Majorof <mks@medialit.ru>
2022-03-05 13:07:46 +0300

    Занятие 4. Задача 1.

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,15 @@
 function makeFriendsList(friends) {
-  // ваш код...
+
+  const ul = document.createElement('ul');
+
+  for (friend of friends) {
+    const li = document.createElement('li');
+
+    li.textContent = friend.firstName + ' ' + friend.lastName;
+    ul.append(li);
+  }
+
+  document.body.append(ul);
+
+  return ul;
 }
