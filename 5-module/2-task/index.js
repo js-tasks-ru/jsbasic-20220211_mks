d7cc163f503ad7d2f6ebaaddc6fe4123b3e80ecf
Kos Majorof <mks@medialit.ru>
2022-03-14 17:14:54 +0300

    Занятие . Задачи 1, 2, 3.

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button');
+  const text = document.querySelector('#text');
+
+  btn.addEventListener('click', () => {
+    if (text.hasAttribute('hidden')) {
+      text.removeAttribute('hidden');
+    } else {
+      text.setAttribute('hidden', '');
+    }
+
+  });
 }
