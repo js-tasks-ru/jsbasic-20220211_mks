23b7eb8e3026f3e72b1de435883cbb75a229b3ef
Kos Majorof <mks@medialit.ru>
2022-03-24 17:13:01 +0300

    Занятие 7. Задачи 1, 2, 3, 4.

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,130 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class RibbonMenu {
+  categories = null;
+  elem = null;
+
   constructor(categories) {
     this.categories = categories;
+    this.elem = this.render();
+  }
+
+  render() {
+    const menu = this.template();
+
+    menu.querySelector('.ribbon__arrow_right').classList.add('ribbon__arrow_visible');
+    
+    menu.querySelector('.ribbon__arrow_right').addEventListener('click', () => {
+      const innerMenu = menu.querySelector('.ribbon__inner');
+      let scrollWidth = innerMenu.scrollWidth;
+      let scrollLeft = innerMenu.scrollLeft;
+      let clientWidth = innerMenu.clientWidth;
+      let scrollRight = scrollWidth - scrollLeft - clientWidth - 350;
+
+      innerMenu.scrollBy(350, 0);
+      
+      innerMenu.addEventListener('scroll', () => {
+        menu.querySelector('.ribbon__arrow_left').classList.add('ribbon__arrow_visible');
+        if (scrollRight < 1) {
+          menu.querySelector('.ribbon__arrow_right').classList.remove('ribbon__arrow_visible');
+        }
+      });
+
+
+    });
+
+    menu.querySelector('.ribbon__arrow_left').addEventListener('click', () => {
+      const innerMenu = menu.querySelector('.ribbon__inner');
+      let scrollLeft = innerMenu.scrollLeft - 350;
+      innerMenu.scrollBy(-350, 0);
+      
+
+      innerMenu.addEventListener('scroll', () => {
+        menu.querySelector('.ribbon__arrow_right').classList.add('ribbon__arrow_visible');
+        if(scrollLeft <= 1) {
+          menu.querySelector('.ribbon__arrow_left').classList.remove('ribbon__arrow_visible');
+        }
+      });
+    });
+
+    return menu;
+  }
+
+  container() {
+    return createElement(`
+    <div class="ribbon"></div>
+    `)
+  }
+
+  itemsMenu() {
+    const innerMenu = createElement(`
+      <div class="ribbon__inner"></div>
+    `)
+    let selectedItemMenu;
+    
+
+    for (this.category of this.categories) {
+      const idItemMenu = this.category.id;
+      const itemMenu = createElement(`
+        <a href="#" class="ribbon__item" data-id="${this.category.id}">${this.category.name}</a>
+      `);
+
+      innerMenu.addEventListener('click', () => {
+        let target = event.target;
+        
+        if (target.tagName != 'A') return;
+  
+        highlight(target);
+      });
+
+      itemMenu.addEventListener('click', () => {
+        const event = new CustomEvent('ribbon-select', {
+          detail: idItemMenu,
+          bubbles: true
+        });
+        this.elem.dispatchEvent(event);
+      });
+      
+      
+  
+      function highlight(td) {
+        if (selectedItemMenu) {
+          selectedItemMenu.classList.remove('ribbon__item_active');
+        }
+        selectedItemMenu = td;
+        selectedItemMenu.classList.add('ribbon__item_active');
+      }
+
+      innerMenu.append(itemMenu);
+    }
+
+    return innerMenu;
+  }
+
+  leftarrowMenu() {
+    return createElement(`
+      <button class="ribbon__arrow ribbon__arrow_left">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+    `)
+  }
+
+  rightarrowMenu() {
+    return createElement(`
+      <button class="ribbon__arrow ribbon__arrow_right">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+    `)
+  }
+
+  template() {
+    const container = this.container();
+    const leftarrowMenu = this.leftarrowMenu();
+    const itemsMenu = this.itemsMenu();
+    const rightarrowMenu = this.rightarrowMenu();
+
+    container.append(leftarrowMenu, itemsMenu, rightarrowMenu);
+
+    return container;
   }
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,64 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.elem = this.template();
+  }
+
+  setTitle(modalTitle) {
+    this.elem.querySelector('.modal__title').innerText = modalTitle;
+  }
+
+  setBody(modalBodyText) {
+    this.elem.querySelector('.modal__body').innerHTML = '';
+    this.elem.querySelector('.modal__body').innerText = modalBodyText.innerText;
+  }
+
+  open() {
+    document.querySelector('.container').append(this.elem);
+    document.body.classList.add('is-modal-open');
+
+    this.elem.querySelector('.modal__close').addEventListener('click', () => {
+      document.body.classList.remove('is-modal-open');
+      this.elem.remove();
+    });
+
+    const onKey = (event) => {
+      if (event.code === 'Escape') {
+        this.elem.remove();
+      }
+    }
+
+    document.body.addEventListener('keydown', onKey);
+    document.body.addEventListener('keyup', (event) => {
+      if (event.code === 'Escape') {
+        document.body.classList.remove('is-modal-open');
+        document.body.removeEventListener('keydown', onKey);
+      }
+    });
+  }
+
+  template() {
+    const modal = createElement(`
+      <div class="modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__inner">
+          <div class="modal__header">
+            <!--Кнопка закрытия модального окна-->
+            <button type="button" class="modal__close">
+              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon">
+            </button>
+            <h3 class="modal__title"></h3>
+          </div>
+          <div class="modal__body"></div>
+        </div>
+      </div>
+    `);
+
+    return modal;
+  }
+
+  close() {
+    document.body.classList.remove('is-modal-open');
+    this.elem.remove();
   }
 }
